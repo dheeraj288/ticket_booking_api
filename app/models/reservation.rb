f34be56c2bc21058c89f357ee650d753dede0c6d
waffer <waffer@gmail.com>
2023-11-06 15:21:05 +0530

    add reservation validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,6 @@
 class Reservation < ApplicationRecord
   belongs_to :user
+  validates :name, :email, :number_of_guests, presence:true
+  validates :email, uniqueness:true
+  validates :date, presence:true
 end
